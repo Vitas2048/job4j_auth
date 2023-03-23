fcbe8c3ad333bb9276382b982089a1a38367a1f9
vitas2048 <vithag97@mail.ru>
2023-03-23 12:51:42 +0700

    6. ResponseEntity [#504799]

--- src/main/java/ru/job4j/controller/UserController.java
+++ src/main/java/ru/job4j/controller/UserController.java
@@ -3,6 +3,8 @@ package ru.job4j.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import ru.job4j.domain.Person;
@@ -25,7 +27,7 @@ public class UserController {
     private BCryptPasswordEncoder encoder;
 
     @PostMapping("/sign-up")
-    public void signUp(@RequestBody Person person) throws Exception {
+    public ResponseEntity<String> signUp(@RequestBody Person person) throws Exception {
         if ("con".equals(person.getUsername())) {
             throw new Exception("You are not Bill Gates");
         }
@@ -34,11 +36,27 @@ public class UserController {
         }
         person.setPassword(encoder.encode(person.getPassword()));
         persons.save(person);
+        var body = new HashMap<>() {{
+            put("key", "value");
+        }}.toString();
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .header("Registration", "successful")
+                .contentType(MediaType.TEXT_PLAIN)
+                .contentLength(body.length())
+                .body(body);
     }
 
     @GetMapping("/all")
-    public List<Person> findAll() {
-        return persons.findAll();
+    public ResponseEntity<String> findAll() {
+        var map = new HashMap<>();
+        persons.findAll().forEach(p -> map.put(p.getId(), p.getUsername()));
+        var body = map.toString();
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .header("List of Person", "successful")
+                .contentType(MediaType.TEXT_PLAIN)
+                .contentLength(body.length())
+                .body(body);
+
     }
 
     @ExceptionHandler(value = {Exception.class})
